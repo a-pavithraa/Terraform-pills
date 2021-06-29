5175a54edecb2ae6602096376b9b5478a71e23fd
a-pavithraa <a.pavithraa@gmail.com>
2021-06-29 17:56:42 +0530

    Delete commands.ps1

--- commands.ps1
+++
@@ -1,6 +0,0 @@
-Start-Transcript -Path 'C:/tomcat/terraform-log.txt' -append;
-$VerbosePreference = 'Continue';
-$InformationPreference = 'Continue';
-$Env:CATALINA_HOME = "C:/tomcat/apache-tomcat-9.0.46-windows-x64/apache-tomcat-9.0.46/"
-C:/tomcat/apache-tomcat-9.0.46-windows-x64/apache-tomcat-9.0.46/bin/startup.bat
-Stop-Transcript;
